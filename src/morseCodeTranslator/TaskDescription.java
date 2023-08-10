ada79c4c09c82e25592cc165aab2eac0e34c2f1f
githublin8888 <chunlinzhou2017@hotmail.com>
2023-08-10 23:23:06 +0200

    add Task description

---
+++ src/morseCodeTranslator/TaskDescription.java
@@ -0,0 +1,16 @@
+package morseCodeTranslator;
+/*
+Uppgift:
+
+Skriv ett program som gör om morsekod till engelska och engelska till morsekod
+
+Skriv programmet med minst en logikklass samt en klass som läser in text och skriver ut text (med en main-metod)
+
+Använd den internationella morsekoden
+
+Utveckla programmet i TDD och skriv minst 3 JUnit-testfall
+
+Skapa minst två felhanteringar i systemet som fångar två saker som användaren kan göra som går utanför det vanliga användandet av systemet​
+
+
+ */
---
+++ src/printText/TaskDescription.java
@@ -0,0 +1,24 @@
+package printText;
+
+/*
+Uppgift:
+
+* Skriv ett program som läser in text ifrån kommandoraden rad för rad tills användaren skriver ordet stop.
+
+* När användaren är klar skriver programmet ut:
+antal tecken;
+hur många rader som användaren har skrivit, exklusive raden med ordet stop;
+antal ord (separerade med blanksteg);
+det längsta ordet.
+
+* Kontrollen om användaren har skrivit ordet stop eller inte måste ligga i den andra ”logik-klassen”
+
+* Till programmet skall minst tre Junit-testfall skrivas
+
+* Testfallen skall skilja sig märkvärt åt och testa olika delar av programmet
+
+* Programmet skall versionshanteras och finnas tillgängligt på Github.
+
+
+
+ */
